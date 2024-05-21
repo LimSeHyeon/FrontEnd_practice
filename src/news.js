ea867558251dacf19bc5f07ef35599b51ab24e18
LimSeHyeon <dlatpgus1205@naver.com>
2024-05-21 23:37:51 +0900

    feat:: 다음뉴스 제목, url 크롤링하기

--- src/news.js
+++ src/news.js
@@ -10,7 +10,7 @@ import fs from 'fs'
     - 저장 형태는 Object형태로 하시되 key는 끝나는 날짜, value는 array<object({title:기사제목, url: 기사링크})>로 저장하기
 */
 
-let news = [];
+let news = {};
 
 const timelineSd = [20240401000000, 20240408000000, 20240408000000];
 const timelineEd = [20240405235959, 20240412235959, 20240415235959];
@@ -31,7 +31,22 @@ async function main(){
             }
         });
         let data = await resp.data;
+        const $ = cheerio.load(data);
+        const newsList = $('ul.c-list-basic > li');
+        const newsParsed = newsList.map((i, elem) => {
+            const anchor = $(elem).find('.c-item-content .item-title a');
+            const title = anchor.text();
+            const url = anchor.prop('href');
+            return { "title": title, "url": url };
+        }).get();
+        const date = timelineEd[i].toString();
+        const year = date.substring(0,4);
+        const month = date.substring(4,6);
+        const day = date.substring(6,8);
+        news[`${year}-${month}-${day}`] = newsParsed;
     }
+    console.log(news);
+    fs.writeFile('../news.json', JSON.stringify(news, null, 2), ()=>{});
 }
 
 main();
